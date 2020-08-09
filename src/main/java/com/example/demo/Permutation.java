ea9c3fe0d72a7feb8a65d99ae28d3b9725a6cd4b
androidjp <1049578026@qq.com>
2020-08-09 22:03:05 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/Permutation.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+/**
+ * @author Jasper Wu
+ * @date 2020/8/9
+ **/
+public class Permutation {
+    public String[] getPermutation(String s) {
+        return null;
+    }
+}
