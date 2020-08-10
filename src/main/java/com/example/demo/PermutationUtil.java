622f0cb1c8bac057764cd0eb2021e32ac2362457
Jasper Wu <jasper.wu@cargosmart.com>
2020-08-10 09:34:05 +0800

    case: one letter list permutations -- refactor

--- src/main/java/com/example/demo/PermutationUtil.java
+++ src/main/java/com/example/demo/PermutationUtil.java
@@ -1,14 +1,15 @@
 package com.example.demo;
 
-import java.util.Arrays;
 import java.util.List;
+
+import static java.util.Collections.singletonList;
 
 /**
  * @author Jasper Wu
  * @date 2020/8/9
  **/
 public class PermutationUtil {
-    public static List<String> getPermutation(String s) {
-        return Arrays.asList(s);
+    public static List<String> listAllPermutation(String letter) {
+        return singletonList(letter);
     }
 }
--- src/test/java/com/example/demo/PermutationUtilTest.java
+++ src/test/java/com/example/demo/PermutationUtilTest.java
@@ -2,19 +2,19 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
 import java.util.List;
 
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PermutationUtilTest {
     @Test
-    public void should_return_a_when_getPermutation_given_one_letter_a() {
+    public void should_return_a_when_listAllPermutation_given_one_letter_a() {
         // given
         String letter = "a";
         // when
-        final List<String> permutations = PermutationUtil.getPermutation(letter);
+        final List<String> permutations = PermutationUtil.listAllPermutation(letter);
         // then
-        assertThat(permutations).isEqualTo(Collections.singletonList("a"));
+        assertThat(permutations).isEqualTo(singletonList("a"));
     }
 }
