b30aaf9c8a1f3acc4c7a46baf6553fc42ab6a7ff
Jasper Wu <jasper.wu@cargosmart.com>
2020-08-10 09:51:32 +0800

    case: list permutations for three letter -- refactor 1

--- src/main/java/com/example/demo/PermutationUtil.java
+++ src/main/java/com/example/demo/PermutationUtil.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -11,21 +12,23 @@ import static java.util.Collections.singletonList;
  **/
 public class PermutationUtil {
     public static List<String> listAllPermutation(String letter) {
+        if (letter.length() == 1) {
+            return singletonList(letter);
+        }
+        List<String> permutations = new ArrayList<String>();
         if (letter.length() == 2) {
-            return asList(
-                    letter,
-                    letter.substring(1, 2) + letter.substring(0, 1)
-            );
+            permutations.add(letter.substring(0, 1) + letter.substring(1, 2));
+            permutations.add(letter.substring(1, 2) + letter.substring(0, 1));
+            return permutations;
         }
         if (letter.length() == 3) {
-            return asList(
-                    letter,
-                    letter.substring(0, 1) + letter.substring(2, 3) + letter.substring(1, 2),
-                    letter.substring(1, 2) + letter.substring(0, 1) + letter.substring(2, 3),
-                    letter.substring(1, 2) + letter.substring(2, 3) + letter.substring(0, 1),
-                    letter.substring(2, 3) + letter.substring(0, 1) + letter.substring(1, 2),
-                    letter.substring(2, 3) + letter.substring(1, 2) + letter.substring(0, 1)
-            );
+            permutations.add(letter.substring(0, 1) + letter.substring(1, 2) + letter.substring(2, 3));
+            permutations.add(letter.substring(0, 1) + letter.substring(2, 3) + letter.substring(1, 2));
+            permutations.add(letter.substring(1, 2) + letter.substring(0, 1) + letter.substring(2, 3));
+            permutations.add(letter.substring(1, 2) + letter.substring(2, 3) + letter.substring(0, 1));
+            permutations.add(letter.substring(2, 3) + letter.substring(0, 1) + letter.substring(1, 2));
+            permutations.add(letter.substring(2, 3) + letter.substring(1, 2) + letter.substring(0, 1));
+            return permutations;
         }
         return singletonList(letter);
     }
